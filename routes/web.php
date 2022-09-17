d4cf2e946def3ee9c6739d5e9d63604144130b82
Auliaslsbylaa <auliaslsbylaa212@gmail.com>
2022-09-17 21:48:06 +0700

    membuat pembagian role

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,12 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+
+Route::group(['middleware' => ['auth', 'role:admin']], function() {
+    Route::get('/admin', function() {
+
+    });
+
+});
+
 require __DIR__.'/auth.php';
